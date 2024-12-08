34593c2a04b9b05bc6b31f372c78d3e9cd34dd3b
carlos-estrada-resume <carlos.estrada.resume@gmail.com>
2024-12-08 17:51:24 +0800

    new commit

--- app.vue
+++ app.vue
@@ -11,9 +11,8 @@
     <div class="navigation-bar">
       <ul>
         <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#blog">Blog</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#download">Download</a></li>
       </ul>
     </div>
   </div>
